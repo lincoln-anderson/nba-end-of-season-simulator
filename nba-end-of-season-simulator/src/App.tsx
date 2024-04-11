a1318e435497755f86fa52254958af465d31dcf8
lincolnAndersonKyros <lincolnanderson@me.com>
2024-04-11 07:42:17 -0500

    adding packages

---
+++ nba-end-of-season-simulator/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { HomePage } from './pages/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <HomePage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ nba-end-of-season-simulator/src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+export function HomePage() {
+  return (
+    <view>
+      <p>hello</p>
+      <p>hello</p>
+    </view>
+  );
+}
