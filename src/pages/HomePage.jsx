bb64c46281ed4df316ff442654a1003f9bca4c83
lincoln anderson <lincolnanderson@me.com>
2024-08-05 12:42:38 -0500

    prettier

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,33 +1,27 @@
-import {  Box, Stack, Typography, } from '@mui/material';
-import { useGetNbaStandings } from '../api/standings.queries';
-import TeamCard from '../components/TeamCard';
+import { Box, Stack, Typography } from '@mui/material'
+import { useGetNbaStandings } from '../api/standings.queries'
+import TeamCard from '../components/TeamCard'
 import data from '../api/exampleStandings.json'
-
 
 export function HomePage() {
   // const { data: standings, isLoading } = useGetNbaStandings('east');
 
-// if (isLoading) {
-//     return <div>Loading...</div>;
-//   }
-  console.log(data);
-  data.sort((a, b) => parseInt(b.win) - parseInt(a.win));
+  // if (isLoading) {
+  //     return <div>Loading...</div>;
+  //   }
+  console.log(data)
+  data.sort((a, b) => parseInt(b.win) - parseInt(a.win))
 
   return (
     <Box>
-    <Stack>
-      {/* <Typography>{data.example1.away.win}</Typography> */}
-    </Stack>
-    <Stack>
-      <Stack gap={8}>
-        {data.map((team) => (
-          <TeamCard props={team} ></TeamCard>
-        ))} 
-
+      <Stack>{/* <Typography>{data.example1.away.win}</Typography> */}</Stack>
+      <Stack>
+        <Stack gap={8}>
+          {data.map(team => (
+            <TeamCard props={team}></TeamCard>
+          ))}
         </Stack>
-  
-      
-    </Stack>
+      </Stack>
     </Box>
-  );
+  )
 }
